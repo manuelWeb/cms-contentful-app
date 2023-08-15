4e970eb3a166d0ec4a9bc76cc44cf423b369df1b
manuelWeb <devries.webdesigner@gmail.com>
2023-08-15 19:01:51 +0200

    fix nav

--- components/nav.js
+++ components/nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import cn from 'classnames'
+import Image from 'next/image';
 
 const nav = () => {
   const router = useRouter();
@@ -23,7 +24,7 @@ const nav = () => {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+          <Image src="https://flowbite.com/docs/images/logo.svg" width={32} height={33} className="h-8 mr-3" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Next 13</span>
         </a>
         <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
